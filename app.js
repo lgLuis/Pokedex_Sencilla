c2c037c0c9a99cf1734bd146f7ea3f73181293af
lgLuis <luis_unlz@yahoo.com.ar>
2020-12-04 17:04:03 -0300

    oki doki

--- app.js
+++ app.js
@@ -5,8 +5,43 @@ async function getPokemon(id){
     return data
 }
 async function init(){
+    const pokemon = await getPokemon(150)
+    updatePokemon(pokemon)
+}
+init()
+
+function updatePokemon(pokemon){
+    window.image.setAttribute('src', pokemon.sprites.front_default)
+    window.pokemon.textContent = pokemon.name
+
+}
+
+window.search.addEventListener('change', async ()=>{
+    const pokemon = await getPokemon(window.search.value)
+    updatePokemon(pokemon)
+
+})
+
+
+
+
+
+
+
+
+
+
+
+/*
+async function getPokemon(id){
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    const data = await response.json()
+    return data
+}
+async function init(){
     const firstPokemon = await getPokemon(150)
     window.image.setAttribute('src', firstPokemon.sprites.front_default)
     window.pokemon.textContent = firstPokemon.name
 }
 init()
+*/
